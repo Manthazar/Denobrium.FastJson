7173ef0cb17e425acbe383aafcedd3f18384bfff
Aron <arko@denobrium.net>
2023-12-12 17:35:31 +0100

    #7 Minor clean up, adding sample

---
+++ Denobrium.Json.BenchmarkConsole/Samples/DataContractSample.cs
@@ -0,0 +1,25 @@
+﻿namespace Denobrium.Json.Benchmark.Samples
+{
+    internal class DataContractSample
+    {
+        public void Sample()
+        {
+            //BenchmarkDataClass c = CreateTestedObject();
+            //BenchmarkDataClass target;
+
+            //Denobrium.Json.Json.Current.DefaultParameters.UseTypeExtension = true;
+            //Denobrium.Json.Json.Current.DefaultParameters.RegisterTypeDescriptor(
+            //    new Denobrium.Json.Registry.DataContractTypeDescriptor(typeof(Benchmarks).Assembly));
+            //string jsonText = Denobrium.Json.Json.Current.ToJson(c);
+
+            //    JsonObject deserializedStore;
+            //    target = new BenchmarkDataClass();
+
+            //    for (int i = 0; i < iterationsPerRun; i++)
+            //    {
+            //        deserializedStore = (JsonObject)Denobrium.Json.Json.Current.ReadJsonValue(jsonText);
+            //        Denobrium.Json.Json.Current.BuildUp(target, deserializedStore);
+            //    }
+        }
+    }
+}
